d9517ba17700e38ee02e652255274e977d24c725
SAG <sahak.agopyan@xerox.com>
2018-12-13 17:27:17 +0100

    modif

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from openpyxl import load_workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 import pandas as pd
 import datetime
-
+import barcode
+from barcode.writer import ImageWriter
 wb = load_workbook('PDM.xlsx')
 # To display all of the available worksheet names
 sheets = wb.sheetnames
@@ -20,7 +21,9 @@ columns=df.loc[1,:]
 datas=df[3:]
 datas.columns=columns
 test_date = datetime.datetime.fromtimestamp(1525132800)
+EAN13=barcode.get('ean13')
 
 dates = datas["Date début d'action"].drop_duplicates()
 for date in dates:
-    print(datas[datas["Date début d'action"]==date])
+    print(datas[datas["Date début d'action"]==date][['Produit','CODE EAN']])
+    ean = EAN13(u'3140100353341', writer=ImageWriter())
